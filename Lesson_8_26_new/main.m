4e7dfc2411dfcdfb1899f470437e1c05b701055b
Tianqi Zhang <1120404010@qq.com>
2015-08-26 14:19:11 +0800

    Initial Commit

---
+++ Lesson_8_26_new/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lesson_8_26_new
+//
+//  Created by 张天琦 on 15/8/26.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
